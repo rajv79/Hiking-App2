088968ce0e658bc0dabb9b9c0f540926b0a4cfcd
Vivek Raj <rajviv79@gmail.com>
2021-01-29 03:34:03 -0500

    Improved login Page

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,600,600);
+			Scene scene = new Scene(root,800,600);
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
